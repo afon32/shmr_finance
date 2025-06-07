41b4398dcf34080c055ace82109527b203bbf9de
afon32 <kozlov.afanasii@mail.ru>
2025-06-07 23:22:13 +0300

    add repositories

---
+++ lib/data/local/sembast_db/sembast_repository.dart
@@ -0,0 +1,9 @@
+import 'package:shmr_finance/data/local/abstract/local_repository.dart';
+
+class SembastRepository implements LocalRepository {
+  
+  @override
+  void compareData() {
+    // TODO: implement compareData
+  }
+}
---
+++ lib/data/network/network_repository.dart
@@ -0,0 +1,3 @@
+import 'package:shmr_finance/data/network/abstract/network_repository.dart';
+
+class NetworkRepositoryImpl implements NetworkRepository {}
