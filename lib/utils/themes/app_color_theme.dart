773c28892c048a55a0d8e0c7df122bed96a2be67
afon32 <kozlov.afanasii@mail.ru>
2025-06-07 21:58:04 +0300

    add themes

---
+++ lib/utils/themes/app_color_theme.dart
@@ -0,0 +1,37 @@
+part of 'app_theme.dart';
+
+extension AppColorTheme on ThemeExtension<ColorTheme> {
+  static ColorTheme? _darkTheme;
+  static ColorTheme get dark => _darkTheme ??= const ColorTheme(
+        backgroundColor: Color(0xFF0F0F0F),
+        containerColor: Color(0xFF1B1B1B),
+        foregroundColor: Color(0xFF2C2C2D),
+        dividerColor: Color(0xFF454545),
+        iconColor: Color(0xFFF4F4F4),
+        textColor: Color(0xFFF4F4F4),
+        secondaryTextColor: Color(0x99DFDFDF),
+        disabled: Color(0xFF494949),
+        mainLogoColor: Color.fromARGB(255, 200, 168, 94),
+        appBarColor: Color.fromARGB(255, 42, 232, 129),
+        selectionItemsColor: Color.fromARGB(255, 212, 250, 230),
+        textContainerColor: Color.fromARGB(255, 68, 235, 144),
+        rejectButtonColor: Color.fromARGB(255, 228, 105, 98),
+      );
+
+  static ColorTheme? _lightTheme;
+  static ColorTheme get light => _lightTheme ??= const ColorTheme(
+        backgroundColor: Color(0xFFFCFCFC),
+        containerColor: Color(0xFFF3F3F3),
+        foregroundColor: Color(0xFFECECEC),
+        dividerColor: Color(0xFFD8D8D8),
+        iconColor: Color(0xFF171717),
+        textColor: Color(0xFF171717),
+        secondaryTextColor: Color(0x993A3A3A),
+        disabled: Color(0xFFECECEC),
+        mainLogoColor: Color.fromARGB(255, 55, 87, 161),
+        appBarColor: Color.fromARGB(255, 42, 232, 129),
+        selectionItemsColor: Color.fromARGB(255, 212, 250, 230),
+        textContainerColor: Color.fromARGB(255, 68, 235, 144),
+        rejectButtonColor: Color.fromARGB(255, 228, 105, 98),
+      );
+}
---
+++ lib/utils/themes/app_theme.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+
+part 'app_color_theme.dart';
+part 'color_theme.dart';
+
+class AppTheme {
+  static ThemeData get lightTheme => ThemeData.light(
+        useMaterial3: true,
+      ).copyWith(
+        actionIconTheme: _actionIconTheme,
+        textTheme: _lightTextTheme,
+        extensions: <ThemeExtension<ColorTheme>>[
+          AppColorTheme.light,
+        ],
+      );
+
+  static ThemeData get darkTheme => ThemeData.dark(
+        useMaterial3: true,
+      ).copyWith(
+        actionIconTheme: _actionIconTheme,
+        textTheme: _darkTextTheme,
+        extensions: <ThemeExtension<dynamic>>[
+          AppColorTheme.dark,
+        ],
+      );
+
+  static final TextTheme _lightTextTheme = Typography.englishLike2021.merge(Typography.blackHelsinki);
+  static final TextTheme _darkTextTheme = Typography.englishLike2021.merge(Typography.whiteHelsinki);
+
+  static final _actionIconTheme = const ActionIconThemeData().copyWith(
+    backButtonIconBuilder: (context) => IconButton(
+      iconSize: 24,
+      onPressed: context.pop,
+      icon: const Icon(Icons.chevron_left),
+    ),
+  );
+}
+
+extension ShmrTheme on BuildContext {
+  ColorTheme get theme => Theme.of(this).extension<ColorTheme>()!;
+  TextTheme get textTheme => Theme.of(this).textTheme;
+}
