d9a7729374ad106c778f6e00db5c7b9154affbee
afon32 <kozlov.afanasii@mail.ru>
2025-07-08 18:56:25 +0300

    implement icons and splash

---
+++ shmr_charts_package/lib/column_chart/shmr_column_chart.dart
@@ -0,0 +1,20 @@
+// import 'package:fl_chart/fl_chart.dart';
+// import 'package:flutter/widgets.dart';
+
+// class ShmrColumnChart extends StatelessWidget {
+//   const ShmrColumnChart({super.key});
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return BarChart(BarChartData(
+//         gridData: const FlGridData(show: false),
+//         titlesData: CustomFlTitlesData(context: context),
+//         borderData: FlBorderData(show: false),
+//         barTouchData: CustomBarTouchData(context: context),
+//         barGroups: viewModel.buys
+//             .map((value) => BarChartGroupData(x: value.month, barRods: [
+//                   BarChartRodData(toY: value.value, color: value.barColor)
+//                 ]))
+//             .toList()));
+//   }
+// }
