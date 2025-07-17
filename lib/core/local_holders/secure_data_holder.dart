7c014d05262790909398468e20e5807d991cedea
afon32 <kozlov.afanasii@mail.ru>
2025-07-17 19:11:08 +0300

    create secure data holder

---
+++ lib/core/local_holders/secure_data_holder.dart
@@ -0,0 +1,17 @@
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import 'models/secure/secure_data.dart';
+
+class SecureDataHolder extends Cubit<SecureData> {
+  SecureDataHolder() : super(SecureData.initial());
+
+  void setPin(String? pin) {
+    final newState = state.copyWith(pin: pin);
+    emit(newState);
+  }
+
+  void setBio(bool isOn) {
+    final newState = state.copyWith(bioIsOn: isOn);
+    emit(newState);
+  }
+}
