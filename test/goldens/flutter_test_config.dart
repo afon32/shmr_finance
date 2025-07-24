ae41cb9e9870a4eade2b300b0cd1f7235b1b647d
afon32 <kozlov.afanasii@mail.ru>
2025-07-24 15:16:42 +0300

    configure environment for golden tests

---
+++ test/goldens/flutter_test_config.dart
@@ -0,0 +1,14 @@
+import 'dart:async';
+
+import 'package:alchemist/alchemist.dart';
+import 'package:flutter/material.dart';
+
+Future<void> testExecutable(FutureOr<void> Function() testMain) async {
+  return AlchemistConfig.runWithConfig(
+    config: AlchemistConfig.current().copyWith(
+      goldenTestTheme: GoldenTestTheme.standard()
+          .copyWith(backgroundColor: Colors.cyan) as GoldenTestTheme?,
+    ),
+    run: testMain,
+  );
+}
